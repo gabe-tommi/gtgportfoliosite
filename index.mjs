b8cee2b27c5a9c199b05db9331281b016c12673b
gabe-tommi <gabetgal2112@gmail.com>
2025-07-05 21:43:58 -0700

    init commit

---
+++ index.mjs
@@ -0,0 +1,43 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+import session from 'express-session';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Middleware for parsing POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse JSON bodies
+
+// Session middleware
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+// Middleware for user authentication
+function userAuth(req, res, next) {
+    if (req.session?.userAuthenticated) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+const pool = mysql.createPool({
+    host: "gabedevspace.com",
+    user: "gabedevs_webuser",
+    password: "(+U%[VpVSy$-",
+    // database: "gabedevs_final", // create db and set it
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
